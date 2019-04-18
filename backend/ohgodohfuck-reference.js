b2d91122a2befa20890b2819875e2629f3eba9e6
Jack <jack.lafond@icloud.com>
2019-04-18 13:26:15 -0400

    begin backend reference file

--- backend/ohgodohfuck-reference.js
+++ backend/ohgodohfuck-reference.js
@@ -1,8 +1,24 @@
 // I have no fucking clue what im doing lets get this breads
 
-const testFolder = '../headers/';
 const fs = require('fs');
 
-fs.readdirSync(testFolder).forEach(file => {
-  console.log(file);
+/* This code lists all code files inside /headers/ */
+const headerFolders = '../headers/';
+
+fs.readdirSync(headerFolders).forEach(file => {
+    console.log(file);
+});
+
+/* This code lists all code files with the term "UI" inside /headers/ */
+fs.readdirSync(headerFolders).forEach(file => {
+    if(file.includes("UI")) {
+        console.log(file);
+    }
+});
+
+/* This code lists all code files starting with the term "UI" inside /headers/ */
+fs.readdirSync(headerFolders).forEach(file => {
+    if(file.startsWith("UI")) {
+        console.log(file);
+    }
 });
