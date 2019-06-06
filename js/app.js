4d804023400e343f59213361b00e491b8806f476
S0n1c <itss0n1cdev@gmail.com>
2019-06-06 16:34:41 -0400

    a lot more chances ayoooo

--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@ var app = new Vue({
         files: [],
         search: "",
         page_title: `iOS`,
-        search_results: []
+        search_results: [],
+        showingFile: false,
+        showFile: {},
+        title: "Search iOS Headers"
     },
     methods: {
         isSearch() {
@@ -35,7 +38,7 @@ var app = new Vue({
         log(str) {
             console.trace('[HeaderFinder]', str);
         },
-        getEndpoint(url, options = {method: "GET"}) {
+        getEndpoint(url, options = {method: "GET"}, type = 1) {
             return new Promise((resolve, reject) => {
               fetch(`${this.db}${url}`, options).then((response) => {
                 if (response.status !== 200) {
@@ -49,7 +52,12 @@ var app = new Vue({
                   }
                 };
                 // Examine the text in the response
-                response.json().then((data) => resolve(data));
+                if(type !== 1) {
+                    response.text().then((data) => resolve(data));
+                } else {
+                    response.json().then((data) => resolve(data));
+                }
+                
               }).catch((err) => reject(err));
             });
           },
@@ -83,111 +91,48 @@ var app = new Vue({
                 this.page_title = `Searching for ${search}`;
 
                 this.search_results = [];
-                this.files.forEach((file) => {
+                this.files.forEach((file, i) => {
                     if(file.toLowerCase().includes(search.toLowerCase())) {
-                        this.search_results.push(file);
+                        this.search_results.push(i);
                     }
                 })
 
-                /*var sValue = document.getElementById("search").value;
-                console.log(sValue);
-                document.getElementById("iosVersion").innerHTML = 'Searching for "' + sValue + '"';
-
-                var headerStrings = document.getElementsByClassName("headerstr");
-                for (var i = 0; i < headerStrings.length; i++) {
-                    if (!headerStrings[i].innerHTML.toLowerCase().includes(sValue.toLowerCase())) {
-                        headerStrings[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none";
-                    } else {
-                        headerStrings[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "block";
-                    }
-                }*/
+            
 
             }
+        },
+        loadFile(i) {
+            var file = this.files[i];
+            var version = this.versions[this.current_version];
+            this.showingFile = true;
+            this.getEndpoint(`${version}/${file}`, null, "text").then((data) => {
+
+                ((done) => {
+                    this.showFile = {
+                        title: file,
+                        version:version,
+                        data: data
+                    }
+                    setTimeout((params) => {
+                        done();
+                    }, 500)
+                   // done()
+                })(() => {
+                    hljs.highlightBlock(document.querySelectorAll("pre code")[0])
+                }) 
+                
+               // console.log(data);
+
+            }).catch((e) => {
+                console.log(e);
+            });
+            
+        },
+        closeFile() {
+            this.showingFile = false;
+            this.showFile = {};
         }
     }
   });
   
   app.init();
-
-/*
-
-  var urlParams = new URLSearchParams(location.search);
-  if (!urlParams.has('ios')) {
-      document.location.href = "//" + document.domain + "?ios=12.1.2";
-  } else {
-      var iosV = urlParams.get('ios');
-      try {
-          document.getElementById(iosV).className += " selected";
-          document.getElementById("iosVersion").innerHTML = "iOS " + iosV;
-          console.log(iosV);
-      } catch (e) {
-          document.location.href = "//" + document.domain + "?ios=12.1.2";
-      }
-
-      loadFiles(iosV);
-  }
-  function setVersion(version) {
-      document.location.href = "//" + document.domain + "?ios=" + version;
-  }
-  function openHeader(name) {
-      document.location.href = "//" + document.domain + "/view?ios=" + document.getElementsByClassName("selected")[0].innerHTML + "&header=" + name;
-  }
-
-  function search() {
-      var sValue = document.getElementById("search").value;
-      console.log(sValue);
-      document.getElementById("iosVersion").innerHTML = 'Searching for "' + sValue + '"';
-
-      var headerStrings = document.getElementsByClassName("headerstr");
-      for (var i = 0; i < headerStrings.length; i++) {
-          if (!headerStrings[i].innerHTML.toLowerCase().includes(sValue.toLowerCase())) {
-              headerStrings[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none";
-          } else {
-              headerStrings[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "block";
-          }
-      }
-  }
-
-  var searchBar = document.getElementById("search");
-
-  searchBar.addEventListener('input', search);
-
-  function loadFiles(v) {
-    fetch(`./headers/${v}.json`)
-      .then(
-        function(response) {
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-
-          // Examine the text in the response
-          response.json().then(function(data) {
-            console.log(data[v]);
-
-            var html = "";
-
-            data[v].forEach((item) => {
-              html += `
-              <div onclick="openHeader('${item}')" class="column is-full-mobile is-full-tablet is-half-desktop" style="background-color: #eee; border-radius: 10px; padding: 20px; margin-bottom: 10px; cursor: pointer">
-                  <table style="width: 100%"><tr>
-                      <td style="width: 15px"><h4 class="title is-4"><i class="fas fa-file" style="margin-right: 15px"></i></h4></td>
-                      <td><h4 class="title is-4 headerstr">${item}</h4></td>
-                      <td style="width: 15px; float: right; margin-left: 10px"><h4 class="title is-4"><i class="fas fa-chevron-right" style="opacity: 0.4"></i></h4></td>
-                  </tr></table>
-              </div>
-              `;
-            });
-
-            document.getElementById("headers").innerHTML = html;
-
-
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
-  }
-*/
